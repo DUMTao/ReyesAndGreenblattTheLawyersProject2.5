5455348b5120d6c2536a2c952edffdfe3e69c878
DUMTao <kreyes@mocs.flsouthern.edu>
2026-03-09 13:45:13 -0400

    No parallelization needed ig hehehehe (someone already figured this out years before me xd)

--- src/MySorter.java
+++ src/MySorter.java
@@ -10,32 +10,35 @@ public class MySorter {
     }
 
     //private static volatile boolean foundSolution = false;
-    //test
+
     //Two methods; the separator method and the quicksort algorithm
     public static void sort(int[] arr){
-        //<<threads go here>>
+        quicksort(arr, 0, arr.length - 1);
     }
 
     private static int arrayPartition(int[] arr, int low, int high){
         //Get the median of the array
-        int pivot = arr.length() / 2;
-        int ptr = (low - 1); //Pointer
+        int pivot = arr[(low + high) / 2];
 
-        //Traverse through all the elements
-        for (int j = low; j < high; j++){
-            if (arr[j] <= pivot){
-                //if element smaller than pivot is found, swap with i++
-                ptr++;
-                int temp = arr[ptr];
-                arr[ptr] = arr[j];
-                arr[j] = temp;
+        while (true){
+            while (arr[low] < pivot){
+                low++;
             }
+            while (arr[high] > pivot){
+                high--;
+            }
+
+            if (low >= high){
+                return high;
+            }
+
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+
+            low++;
+            high--;
         }
-
-        int temp = arr[ptr + 1];
-
-
-        return;
     }
 
     private static void quicksort(int[] arr, int low, int high){
@@ -45,11 +48,24 @@ public class MySorter {
             int pivot = arrayPartition(arr, low, high);
 
             //recursive; on the left of pivot
-            quicksort(arr, low, pivot - 1);
+            quicksort(arr, low, pivot);
 
             //recursive; on the right of pivot
             quicksort(arr, pivot + 1, high);
         }
     }
+
+    /*
+    //Traverse through all the elements
+        for (int j = low; j < high; j++){
+            if (arr[j] <= pivot){
+                //if element smaller than pivot is found, swap with i++
+                ptr++;
+                int temp = arr[ptr];
+                arr[ptr] = arr[j];
+                arr[j] = temp;
+            }
+        }
+     */
 
 }
